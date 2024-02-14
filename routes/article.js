df42aa6cba1ebb722c7dd949cad3016e31dafbb8
StenKuusk <sten.kuusk@voco.ee>
2024-02-14 11:38:35 +0200

    Getted article data by slug

--- routes/article.js
+++ routes/article.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/article');
 
-router.get('/', articleController.getAllArticles)
+router.get('/', articleController.getAllArticles);
+router.get('/article/:slug', articleController.getArticleBySlug);
 
 module.exports = router;
